8267d1588546ecc8740413313920da0b3a3c7718
TsinjoNantosoa <tsinjonantosoa@gmail.com>
2025-06-24 16:09:11 +0300

    Modification

--- jobs/main.py
+++ jobs/main.py
@@ -2,7 +2,6 @@ import os
 import random
 import uuid
 from datetime import datetime, timedelta
-
 from confluent_kafka import SerializingProducer
 import simplejson as json
 
@@ -19,6 +18,7 @@ TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
 WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_topic')
 EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_topic')
 
+random.seed(42)
 start_time = datetime.now()
 start_location = LONDON_COORDINATES.copy()
 
@@ -34,31 +34,55 @@ def generate_gps_data(device_id, timestamp, vehicle_type='private'):
         'id': str(uuid.uuid4()),
         'deviceId': device_id,
         'timestamp': timestamp,
-        'speed': random.uniform(0, 40),  # vitesse in km/h
+        'speed': random.uniform(0, 40),
         'direction': 'North-East',
         'vehicle': vehicle_type
     }
 
 
-def generate_traffic_camera_data(device_id, timestamp, camera_id):
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id):
     return {
-        'id': uuid.uuid4(),
+        'id': str(uuid.uuid4()),
         'deviceId': device_id,
+        'location': location,
         'cameraId': camera_id,
         'timestamp': timestamp,
-        'snapshot': 'Based64EncodeString'
+        'snapshot': 'Base64EncodeString'
+    }
 
+
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        'id': str(uuid.uuid4()),
+        'deviceId': device_id,
+        'location': location,
+        'timestamp': timestamp,
+        'temperature': random.uniform(5, 26),
+        'weatherCondition': random.choice(['Sunny', 'Cloudy', 'Rain', 'Snow']),
+        'precipitation': random.uniform(0, 25),
+        'windSpeed': random.uniform(0, 100),
+        'humidity': random.randint(0, 100),
+        'airQualityIndex': random.uniform(0, 500)
+    }
+
+
+def generate_emergency_incident_data(device_id, timestamp, location):
+    return {
+        'id': str(uuid.uuid4()),
+        'deviceId': device_id,
+        'incidentId': str(uuid.uuid4()),
+        'type': random.choice(['Accident', 'Fire', 'Medical', 'Police', 'None']),
+        'timestamp': timestamp,
+        'location': location,
+        'status': random.choice(['Active', 'Resolved']),
+        'description': 'This is the description of the incident.'
     }
 
 
 def simulate_vehicle_movement():
     global start_location
-
-    start_location['latitude'] += LATITUDE_INCREMENT
-    start_location['longitude'] += LONGITUDE_INCREMENT
-
-    start_location['latitude'] += random.uniform(-0.0005, 0.0005)
-    start_location['longitude'] += random.uniform(-0.0005, 0.0005)
+    start_location['latitude'] += LATITUDE_INCREMENT + random.uniform(-0.0005, 0.0005)
+    start_location['longitude'] += LONGITUDE_INCREMENT + random.uniform(-0.0005, 0.0005)
     return start_location
 
 
@@ -78,15 +102,28 @@ def generate_vehicle_data(device_id):
     }
 
 
-def simulate_journey(producer, device_id):
-    while True:
-        vehicle_data = generate_vehicle_data(device_id)
-        gps_data = generate_gps_data(device_id, vehicle_data['timestamp'])
-        traffic_data_camera = generate_traffic_camera_data(device_id,vehicle_data['timestamp'], 'Tsinjo-Cam123')
-        print(vehicle_data)
-        # Pour l’envoyer à Kafka, tu peux ajouter ceci si nécessaire :
-        # producer.produce(topic=VEHICLE_TOPIC, value=json.dumps(vehicle_data))
-        break
+def simulate_journey(device_id, producer):
+    vehicle_data = generate_vehicle_data(device_id)
+    gps_data = generate_gps_data(device_id, vehicle_data['timestamp'])
+    traffic_data_camera = generate_traffic_camera_data(device_id, vehicle_data['timestamp'], vehicle_data['location'],
+                                                       'Tsinjo-Cam123')
+    weather_data = generate_weather_data(device_id, vehicle_data['timestamp'], vehicle_data['location'])
+    emergency_incident_data = generate_emergency_incident_data(device_id, vehicle_data['timestamp'],
+                                                               vehicle_data['location'])
+
+    print(vehicle_data)
+    print(gps_data)
+    print(traffic_data_camera)
+    print(weather_data)
+    print(emergency_incident_data)
+
+    # Envoi Kafka
+    producer.produce(topic=VEHICLE_TOPIC, value=json.dumps(vehicle_data))
+    producer.produce(topic=GPS_TOPIC, value=json.dumps(gps_data))
+    producer.produce(topic=TRAFFIC_TOPIC, value=json.dumps(traffic_data_camera))
+    producer.produce(topic=WEATHER_TOPIC, value=json.dumps(weather_data))
+    producer.produce(topic=EMERGENCY_TOPIC, value=json.dumps(emergency_incident_data))
+    producer.flush()
 
 
 if __name__ == "__main__":
@@ -97,8 +134,8 @@ if __name__ == "__main__":
     producer = SerializingProducer(producer_config)
 
     try:
-        simulate_journey(producer, 'Vehicle_tsinjo-123')
+        simulate_journey("tsinjo-device-001", producer)
     except KeyboardInterrupt:
-        print('The simulation was ended by the user.')
+        print('Simulation interrompue par l’utilisateur.')
     except Exception as e:
-        print(f'Unexpected error occurred: {e}')
+        print(f'Erreur inattendue : {e}')
